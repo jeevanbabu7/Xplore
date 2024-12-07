55cb3ed7cbcbcc242901f503fb515ae4f38b8c7f
Zidan <muhdzidan007@gmail.com>
2024-12-07 12:03:19 +0530

    merged

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Navbar from "../components/Navbar";
-// import About from "../pages/About";
 import About from "../pages/About";
 // import Events from "../pages/Events";
 // import Workshops from "../pages/Workshops";
