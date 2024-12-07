ba0f7bcc0c7c4fb194b7b76ee8bba5d346f469f2
jeevanbabu7 <jeevanbabu2710@gmail.com>
2024-12-07 21:25:35 +0530

    event

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,42 +1,24 @@
-import React from "react";
-import { useSpring, animated } from "@react-spring/web";
-import eventBanner from "../assets/images/event-banner.png";
-import Event from "../components/Event";
-import { eventData } from "../utils/eventData.js";
-
+import React from 'react'
+import eventBanner from '../assets/images/event-banner.png'
+import Event from '../components/Event'
+import {eventData} from '../utils/eventData.js'
 const Events = () => {
-  // Define the animation using useSpring
-  const bannerAnimation = useSpring({
-    from: { opacity: 0, transform: "scale(0.9)" },
-    to: { opacity: 1, transform: "scale(1)" },
-    config: { tension: 100, friction: 20 },
-  });
-
   return (
     <>
-      {/* Animated Banner Section */}
-      <animated.section
-        className="w-full h-48 md:h-[280px] bg-cover bg-center bg-no-repeat"
-        style={{
-          ...bannerAnimation,
-          backgroundImage: `url(${eventBanner})`,
-        }}
-      ></animated.section>
+        <section className="w-full h-48 md:h-[280px] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${eventBanner})` }}>
+         
+        </section>
+        <div style={{
+        background: 'linear-gradient(159deg, rgba(253,1,48,1) 0%, rgba(200,1,38,1) 0%, rgba(173,1,33,1) 0%, rgba(160,1,31,1) 0%, rgba(149,1,29,1) 0%, rgba(141,1,27,1) 0%, rgba(126,1,24,1) 0%, rgba(89,0,17,1) 0%, rgba(140,1,27,1) 0%, rgba(62,1,12,1) 0%, rgba(90,1,18,1) 0%, rgba(90,1,18,1) 0%, rgba(91,1,18,1) 0%, rgba(71,1,14,1) 21%, rgba(36,0,7,1) 36%, rgba(0,0,0,1) 55%, rgba(0,0,0,1) 91%)',
+      }} className='w-full h-screen overflow-auto p-10 flex flex-row flex-wrap justify-center gap-20'>
+          {eventData.map((event, index) => {
+            return <Event key={index} eventDetails={event} />
+          })}
+        </div>
 
-      {/* Event List */}
-      <div
-        style={{
-          background:
-            "linear-gradient(159deg, rgba(253,1,48,1) 0%, rgba(200,1,38,1) 0%, rgba(173,1,33,1) 0%, rgba(160,1,31,1) 0%, rgba(149,1,29,1) 0%, rgba(141,1,27,1) 0%, rgba(126,1,24,1) 0%, rgba(89,0,17,1) 0%, rgba(140,1,27,1) 0%, rgba(62,1,12,1) 0%, rgba(90,1,18,1) 0%, rgba(90,1,18,1) 0%, rgba(91,1,18,1) 0%, rgba(71,1,14,1) 21%, rgba(36,0,7,1) 36%, rgba(0,0,0,1) 55%, rgba(0,0,0,1) 91%)",
-        }}
-        className="w-full h-screen overflow-auto p-10 flex flex-row flex-wrap justify-center gap-20"
-      >
-        {eventData.map((event, index) => {
-          return <Event key={index} eventDetails={event} />;
-        })}
-      </div>
+   
     </>
-  );
-};
+  )
+}
 
-export default Events;
+export default Events
