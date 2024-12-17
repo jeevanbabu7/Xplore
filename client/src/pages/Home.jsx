ba57a7039582efdf3dd19202f109c39c0e8abc66
jeevanbabu7 <jeevanbabu2710@gmail.com>
2024-12-17 22:33:13 +0530

    navbar fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,22 +9,14 @@ import ContactPage from "./Contact";
 const Home = () => {
   const navigate = useNavigate();
 
-  // Smooth scroll function
-  const scrollToSection = (id) => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-  };
+ 
 
   return (
     <div
-      className="h-screen overflow-y-scroll 
-      scrollbar-thin scrollbar-thumb-gray-300/50 scrollbar-track-gray-100/20 
-      dark:scrollbar-thumb-neutral-500/50 dark:scrollbar-track-neutral-700/20"
+      className="h-screen"
       id="home"
     >
-      <ParallaxLayer scrollToSection={scrollToSection} />
+      <ParallaxLayer />
       <section className="z-50 mt-5">
         <div id="about">
           <AboutPage />
