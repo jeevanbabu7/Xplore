ab617cddfb4c913863b4275d157b4233b55f1c37
Zidan <muhdzidan007@gmail.com>
2025-01-19 17:30:50 +0530

    some changes

--- client/src/pages/Directions.jsx
+++ client/src/pages/Directions.jsx
@@ -17,7 +17,7 @@ const GetDirections = () => {
         {/* Left Column: Map */}
         <div className="flex justify-center w-full md:w-1/2 mb-12 md:mb-0">
           <div className="w-full h-80 md:h-[400px] rounded-lg shadow-lg overflow-hidden">
-            <iframe
+            {/* <iframe
               src="https://www.google.com/maps/embed/v1/place?q=11.986001564305914,75.38173836857075&key=AIzaSyCjngIi8RzMqhJVGj2egMNQDRTmKOwDUWY"
               width="100%"
               height="100%"
@@ -26,7 +26,15 @@ const GetDirections = () => {
               allowFullScreen=""
               aria-hidden="false"
               tabIndex="0"
-            ></iframe>
+            ></iframe> */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d7805.671479557941!2d75.37652375869136!3d11.985865400000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x3ba43e5eaa1932c3%3A0xa686759a5aa8afa5!2sGCE%20KANNUR%2C%20Government%20College%20of%20Engineering%20Kannur%2C%20Dharmasala%2C%20Kerala%20670563!3m2!1d11.9858654!2d75.3816736!5e0!3m2!1sen!2sin!4v1737287641591!5m2!1sen!2sin" 
+                width="600" 
+                height="450" 
+                // style="border:0;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
           </div>
         </div>
 
